74831b418f845a5f047ee35348979c6ad481548b
Maximilian Kautzsch <maximilian.kautzsch@icloud.com>
2024-06-08 19:52:08 +0200

    Implemented vectors into the library.q
    - Used vectors instead of arrays, simplyfing the calculations.
    - Wrote header file for skiplist nodes and skiplists.
    - Implemented readMovingMedian() method by using indexed skiplists.

--- examples/Filters/Filters.ino
+++ examples/Filters/Filters.ino
@@ -8,11 +8,7 @@
 
 #include "MovingAverage.h"
 
-<<<<<<< Updated upstream
-MovingAverage<uint8_t, uint8_t> filter;  // Create instance of the moving average class for filtering the data
-=======
 MovingAverage<> filter;  // Create instance of the moving average class for filtering the data
->>>>>>> Stashed changes
 
 void setup() {
   Serial.begin(9600);  // Initialize serial communication
@@ -20,22 +16,15 @@ void setup() {
 }
 
 void loop() {
-  uint8_t data_point = random(1, 100);  // Generate a random integer between 1 and 100
-  filter.add(data_point);               // Add the data point to the filter
+  int data_point = random(1, 100);  // Generate a random integer between 1 and 100
+  filter.add(data_point);           // Add the data point to the filter
 
   // Calculate different types of average values
-<<<<<<< Updated upstream
-  uint8_t simple_moving_average = filter.readAverage(10);
-  uint8_t cumulative_average = filter.readCumulativeAverage();
-  uint8_t weighted_moving_average = filter.readWeightedAverage(10);
-  uint8_t exponential_moving_average = filter.readExponentialAverage(0.30);
-=======
   int simple_moving_average = filter.readAverage(10);
   int weighted_moving_average = filter.readWeightedAverage(10);
   int moving_median = filter.readMovingMedian(10);
   int exponential_moving_average = filter.readExponentialAverage(0.30);
   int cumulative_average = filter.readCumulativeAverage();
->>>>>>> Stashed changes
 
   filter.print();  // Print all of the computed output values
   delay(10);       // Wait 10ms between every iteration
